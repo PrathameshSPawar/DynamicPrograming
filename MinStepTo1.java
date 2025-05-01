f46e164149b775b04fd4bc543644ad5a077cd653
Prathamesh Pawar <163292716+PrathameshSPawar@users.noreply.github.com>
2025-05-01 18:08:29 +0530

    Update MinStepTo1.java

--- MinStepTo1.java
+++ MinStepTo1.java
@@ -23,24 +23,39 @@ public static void main(String[] args) {
 	}
 
 //here is recursive,memorizing solution
-public static int n=(int)1e6+5;
+    public static int n=(int)1e6+5;
     public static int memo[]=new int[n];
     static{
         Arrays.fill(memo,-1);
     }
-    public static int recursive(int n)
+    public static int top_down(int n)
     {
         if(n==1) return 0;
         
         if(memo[n]!=-1) return memo[n];
         
-        int ans=recursive(n-1)+1;
-        if(n%2==0) ans=Math.min(ans,recursive(n/2)+1);
-        if(n%3==0) ans=Math.min(ans,recursive(n/3)+1);
+        int ans=(n-1)+1;
+        if(n%2==0) ans=Math.min(ans,top_down(n/2)+1);
+        if(n%3==0) ans=Math.min(ans,top_down(n/3)+1);
         return memo[n]=ans;
     }
 	public static void main(String[] args) {
-		System.out.println(recursive(10));
+		System.out.println(top_down(10));
 	}
 //TC: O(N)
 //ASC: O(N*N) callstack,memo
+
+//here is using tabulation 
+public static int bottom_up(int n)
+    {
+        int memo[]=new int[n+1];
+        memo[1]=0;
+        for(int i=2;i<=n;i++)
+        {
+            memo[i]=memo[i-1]+1;
+            if(i%2==0) memo[i]=Math.min(memo[i],memo[i/2]+1);
+            if(i%3==0) memo[i]=Math.min(memo[i],memo[i/3]+1);
+        }
+        return memo[n];
+        
+    }
